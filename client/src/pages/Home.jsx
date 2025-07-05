756c7e7e85d9addc4ac6871a26ca14249f279141
Ansh Patel <anshpatel7623@gmail.com>
2025-07-05 10:21:22 +0530

    blog and home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
+import Bloglist from '../components/Bloglist'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    <Bloglist/>
+    </>
+  )
+}
+
+export default Home
