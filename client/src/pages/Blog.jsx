c7a901e4ea3993e6c3edf7dd1cda08700690c5a1
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-09 19:41:07 +0530

    Implement API calls for fetching blog data and comments, and add error handling with toast notifications

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,36 +1,68 @@
-import React, { use, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
 import { assets, blog_data, comments_data } from '../assets/assets'
 import Navbar from '../components/Navbar'
 import Moment from 'moment'
 import Footer from '../components/Footer'
 import Loader from '../components/Loader'
+import { useAppContext } from '../context/AppContext'
+import toast from 'react-hot-toast'
 
 const Blog = () => {
+
   const {id} = useParams()
+
+  const {axios} = useAppContext()
+
   const [data, setData] = useState(null)
   const [comments, setComments] = useState([])
   const [name, setName] = useState('')
   const [content, setContent] = useState('')
 
-  const fetchBlogData=async ()=>{
-    const data=blog_data.find((item)=>item._id===id)
-    setData(data)
-  }
-
-  const fetchComments=async()=>{
-    setComments(comments_data)
-  }
-
-  const addComment=async()=>{
-    e.preventDefault();
+  const fetchBlogData = async ()=>{
+    try {
+      const {data} = await axios.get(`/api/blog/${id}`)
+      data.success ? setData(data.blog) : toast.error(data.message)
+    } catch (error) {
+      toast.error(error.message)
     }
+  }
+
+  const fetchComments = async () =>{
+    try {
+      const { data } = await axios.post('/api/blog/comments', {blogId: id})
+      if (data.success){
+        setComments(data.comments)
+      }else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
+  const addComment = async (e)=>{
+    e.preventDefault();
+    try {
+      const { data } = await axios.post('/api/blog/add-comment', {blog: id, name, content});
+      if (data.success){
+        toast.success(data.message)
+        setName('')
+        setContent('')
+      }else{
+         toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
   useEffect(()=>{
     fetchBlogData()
     fetchComments()
   },[])
-    
- return data ? (
+
+  return data ? (
     <div className='relative'>
       <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-1 opacity-50'/>
       
@@ -91,10 +123,7 @@ const Blog = () => {
       <Footer/>
 
     </div>
-  ) : (
-    <Loader/>
-  )
+  ) : <Loader/>
 }
 
 export default Blog
-
