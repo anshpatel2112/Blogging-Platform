8e0661d57305f21a3b449d192334d42df2f760a1
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-09 15:34:31 +0530

    Refactor AppContext component for improved code formatting and consistency

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,27 +1,27 @@
-import {createContext, useContext, useEffect, useState} from 'react'
+import { useContext, useEffect, useState } from "react";
+import { createContext } from "react";
 import axios from "axios";
-import {useNavigate} from 'react-router-dom'
-import toast from 'react-hot-toast';
+import {useNavigate} from "react-router-dom";
+import toast from "react-hot-toast";
 
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
+
 const AppContext = createContext();
+export const AppProvider = ({children}) =>{
 
-export const AppProvider = ({ children })=>{
-
-    const navigate = useNavigate()
-
-    const [token, setToken] = useState(null)
-    const [blogs, setBlogs] = useState([])
-    const [input, setInput] = useState("")
+    const navigate = useNavigate();
+    const [token , setToken] = useState(null);
+    const [blogs,setBlogs] = useState([]);
+    const [input,setInput] = useState("");
 
     const fetchBlogs = async ()=>{
         try {
-           const {data} = await axios.get('/api/blog/all');
-           data.success ? setBlogs(data.blogs) : toast.error(data.message)
+           const {data}= await axios.get('/api/blog/all');
+           data.success ? setBlogs(data.blogs) : toast.error(data.message);
         } catch (error) {
-            toast.error(error.message)
+            toast.error(error.message);
         }
     }
 
@@ -30,21 +30,22 @@ export const AppProvider = ({ children })=>{
         const token = localStorage.getItem('token')
         if(token){
             setToken(token);
-            axios.defaults.headers.common['Authorization'] = `${token}`;
+            axios.defaults.headers.common['Authorization'] =` ${token}`
         }
     },[])
+    
+            const value  = {
+                axios,navigate,token,setToken,blogs,setBlogs,input,setInput
+            }
+    return(
 
-    const value = {
-        axios, navigate, token, setToken, blogs, setBlogs, input, setInput
-    }
 
-    return (
         <AppContext.Provider value={value}>
             {children}
         </AppContext.Provider>
     )
 }
 
-export const useAppContext = ()=>{
+export const  useAppContext =()=>{
     return useContext(AppContext)
-};
+}
