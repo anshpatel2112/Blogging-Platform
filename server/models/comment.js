a645838ac6a99b96e598e1458499d930b9b4e9a0
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-10 20:52:11 +0530

    Fix reference case in comment schema for blog field

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
-   blog:{type:mongoose.Schema.Types.ObjectId, ref: 'blog' , required: true},
+   blog:{type:mongoose.Schema.Types.ObjectId, ref: 'Blog' , required: true},
    name:{type:String , required : true},
    content : {type:String , required:true},
    isApproved : {type : Boolean , default:false}//whenever new comment will be posted by default it will be unapproved we have to approved it from admin dashboard
