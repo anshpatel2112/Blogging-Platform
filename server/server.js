faa446fef37b3af916033f54beb29e0792c208ba
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-11 22:49:32 +0530

    Add HTTP methods to CORS middleware configuration

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ await connectDB();
 //Middlewares
 app.use(cors({
   origin: "https://blogging-platform-one-chi.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
 app.use(express.json())
