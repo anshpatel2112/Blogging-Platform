e6ba3c77ef0ebac528db6d04e493c4ee49b4a361
Ankit Rajendra Bhagat <130834974+bhagatankit05@users.noreply.github.com>
2025-07-24 16:36:47 +0530

    Update ProtectedRoute.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,17 +1,1 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { useAppContext } from '../context/AppContext';
 
-const ProtectedRoute = ({ children }) => {
-    const { token, user } = useAppContext();
-
-    // If no token or user, redirect to login
-    if (!token || !user) {
-        return <Navigate to="/login" replace />;
-    }
-
-    // If authenticated, render the protected component
-    return children;
-};
-
-export default ProtectedRoute;
