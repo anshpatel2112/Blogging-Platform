c2dd0434e35a628371a783cdff928930fc786a08
Ankit Rajendra Bhagat <130834974+bhagatankit05@users.noreply.github.com>
2025-07-24 16:40:27 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,30 +7,22 @@ import Dashboard from './pages/admin/Dashboard'
 import AddBlog from './pages/admin/AddBlog'
 import ListBlog from './pages/admin/ListBlog'
 import Comments from './pages/admin/Comments'
-import AdminLogin from './components/admin/Login'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
-import ProtectedRoute from './components/ProtectedRoute'
+import Login from './components/admin/Login'
 import 'quill/dist/quill.snow.css'
 import {Toaster} from 'react-hot-toast'
 import { useAppContext } from './context/AppContext'
 
 const App = () => {
 
-  const {token, user} = useAppContext()
-
-  // Check if user is admin
-  const isAdmin = user?.role === 'admin';
+  const {token} = useAppContext()
 
   return (
     <div>
       <Toaster/>
       <Routes>
-        <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path='/blog/:id' element={<ProtectedRoute><Blog/></ProtectedRoute>} />
-        <Route path='/admin' element={token && isAdmin ? <Layout/> : <AdminLogin/>}>
+        <Route path='/' element={<Home/>} />
+        <Route path='/blog/:id' element={<Blog/>} />
+        <Route path='/admin' element={token ? <Layout/> : <Login/>}>
           <Route index element={<Dashboard/>}/>
           <Route path='addBlog' element={<AddBlog/>}/>
           <Route path='listBlog' element={<ListBlog/>}/>
