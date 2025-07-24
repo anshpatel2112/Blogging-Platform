1cf221953c7094ce74db75b1b0e92e60f328a846
Ankit Rajendra Bhagat <130834974+bhagatankit05@users.noreply.github.com>
2025-07-24 16:46:17 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,15 +1,1 @@
-import express from 'express';
-import { registerUser, loginUser, getUserProfile, logoutUser } from '../controllers/userController.js';
-import auth from '../middleware/auth.js';
 
-const userRouter = express.Router();
-
-// Public routes
-userRouter.post('/register', registerUser);
-userRouter.post('/login', loginUser);
-
-// Protected routes
-userRouter.get('/profile', auth, getUserProfile);
-userRouter.post('/logout', auth, logoutUser);
-
-export default userRouter;
