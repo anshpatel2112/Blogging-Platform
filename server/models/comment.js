5df4ae1a5c21b74f8dd930c07e9c99b9e46adc7b
bhagatankit05 <bhagatankit82@gmail.com>
2025-07-25 15:18:06 +0530

    Refactor code formatting in blog and comment models, and blog routes for improved readability

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
-   blog:{type:mongoose.Schema.Types.ObjectId, ref: 'Blog' , required: true},
-   name:{type:String , required : true},
-   content : {type:String , required:true},
-   isApproved : {type : Boolean , default:false}//whenever new comment will be posted by default it will be unapproved we have to approved it from admin dashboard
-},{timestamps : true});
+    blog: {type: mongoose.Schema.Types.ObjectId, ref: 'Blog', required: true},
+    name:  { type: String, required: true },
+    content: { type: String, required: true },
+    isApproved: { type: Boolean, default: false }, //whenever new comment will be posted by default it will be unapproved we have to approved it from admin dashboard
+},{timestamps: true});
 
-const Comment = mongoose.model('Comment',commentSchema);
+const Comment = mongoose.model('Comment', commentSchema);
 
 export default Comment;
